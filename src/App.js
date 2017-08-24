75c18103d6e938e5acf30096a4b3b6ec7184d07e
stripodi <stripodi11@gmail.com>
2017-08-24 11:38:44 +0930

    Demo Page Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavbarCustom from "./component/NavbarCustom";
 import Header from "./component/Header";
-import About from "./component/About";
-import Service from "./component/Service";
-import Team from "./component/Team";
-import Footer from "./component/Footer";
 
 
 class App extends Component {
@@ -13,12 +8,7 @@ class App extends Component {
         const styleTest = {height:"100%", width: "100%"};
         return (
             <div style={styleTest}>
-            <NavbarCustom />
             <Header />
-            <About />
-            <Service />
-            <Team />
-            <Footer />
             </div>
         );
     }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import logo from "../image/ts.svg";
+
+                    // <div className="col-xs-2">
+                    //     <img src={logo} className="App-logo img-responsive" alt="logo" />
+                    // </div>
+                    // <div className="col-xs-10 vcenter">
+                    //     <div className="vcenter">
+                    //         <h2>
+                    //             Tripodi 
+                    //             <br>
+                    //             </br> 
+                    //             Studios
+                    //         </h2>
+                    //     </div>
+                    // </div>
 
 export default class Header extends Component {
     constructor(props) {
@@ -8,12 +23,17 @@ export default class Header extends Component {
 
     render() {
         return (
-        <header id="header" className="header bg-white container-fluid">
-            <div className="container-fluid">
-                <h1>HEADER</h1>
+        <header id="header" className="header container-fluid">
+            <div className="row">
+                <div className="container-fluid">
+                        <img src={logo} className="App-logo img-responsive vcenter" alt="logo" />
+                        <div className="col-xs-12">
+                            <h2 className="tripodi-title-main text-center">Tripodi</h2>
+                            <h2 className="tripodi-title-bottom text-center">Studios</h2>
+                        </div>
+                </div>
             </div>
         </header>
         )
     }
 }
-
